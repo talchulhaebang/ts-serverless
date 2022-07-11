153d43f7027d318a0110bd7c5346c17f733be7e5
김희재 <heejae.kim@toss.im>
2022-07-11 19:36:40 +0900

    chore: typescript, ramda 버전 업

--- test/ts-serverless.test.ts
+++ test/ts-serverless.test.ts
@@ -1,17 +1,48 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
 // import * as TsServerless from '../lib/ts-serverless-stack';
+import { Result } from "aws-cdk-lib/aws-stepfunctions";
+import R from "ramda";
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/ts-serverless-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new TsServerless.TsServerlessStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test("SQS Queue Created", async () => {
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new TsServerless.TsServerlessStack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
+  const result = await R.pipeWith(andThen, [process1, process2, process3])(
+    "STRING"
+  );
+  console.log("!!!!!!");
+  console.log(result);
 });
+
+function andThen(fn: any, res: any) {
+  return isPromise(res) ? res.then(fn) : fn(res);
+}
+
+export function isPromise(target: any): target is Promise<any> {
+  return (
+    target instanceof Promise ||
+    (typeof target?.then === "function" && typeof target?.catch === "function")
+  );
+}
+
+function process1(str: string) {
+  return str;
+}
+async function process2(str: string) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(str);
+    }, 2000);
+  });
+}
+function process3(str: string) {
+  return `${str} TEST`;
+}
